71b646b08db608b5bb260fba0a5ebc3475a708ae
timolated <thonphet2312@gmail.com>
2025-02-11 21:36:15 +0100

    hardcoded but better sync

--- sync/sync.go
+++ sync/sync.go
@@ -16,11 +16,11 @@ func Testsynclambda() {
 		log.Fatal("Failed to create Minio client")
 	}
 	// sync bucket "fis"
-	downloadStaticFiles(client)
+	syncStaticFiles(client)
 }
 
-func downloadStaticFiles(client *minio.Client) {
-	// Download the static files from the bucket
+func syncStaticFiles(client *minio.Client) {
+	// Sync the static files from the bucket
 	// "fis" to the local filesystem
 	// Define the bucket and local directory
 	bucketName := "fis"
@@ -30,23 +30,67 @@ func downloadStaticFiles(client *minio.Client) {
 	ctx := context.Background()
 	objectCh := client.ListObjects(ctx, bucketName, minio.ListObjectsOptions{Recursive: true})
 
+	// Track the objects in the bucket
+	bucketObjects := make(map[string]struct{})
+
 	for object := range objectCh {
 		if object.Err != nil {
 			log.Fatalln(object.Err)
 		}
 
-		// Download each object
+		// Define the local file path
 		localFilePath := filepath.Join(localDir, object.Key)
-		err := os.MkdirAll(filepath.Dir(localFilePath), os.ModePerm)
-		if err != nil {
-			log.Fatalln(err)
+
+		// Check if the local file exists and is up-to-date
+		shouldDownload := true
+		if fileInfo, err := os.Stat(localFilePath); err == nil {
+			// Compare the modification time
+			objectModTime := object.LastModified
+			localModTime := fileInfo.ModTime()
+			if localModTime.After(objectModTime) || localModTime.Equal(objectModTime) {
+				shouldDownload = false
+			}
 		}
 
-		err = client.FGetObject(ctx, bucketName, object.Key, localFilePath, minio.GetObjectOptions{})
-		if err != nil {
-			log.Fatalln(err)
+		if shouldDownload {
+			// Create the local directory if it doesn't exist
+			err := os.MkdirAll(filepath.Dir(localFilePath), os.ModePerm)
+			if err != nil {
+				log.Fatalln(err)
+			}
+
+			// Download the object
+			err = client.FGetObject(ctx, bucketName, object.Key, localFilePath, minio.GetObjectOptions{})
+			if err != nil {
+				log.Fatalln(err)
+			}
+
+			fmt.Printf("Successfully synced %s\n", object.Key)
+		} else {
+			fmt.Printf("Already up-to-date: %s\n", object.Key)
 		}
 
-		fmt.Printf("Successfully downloaded %s\n", object.Key)
+		// Add the object to the map
+		bucketObjects[localFilePath] = struct{}{}
+	}
+
+	// Remove local files that are not in the bucket
+	err := filepath.Walk(localDir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if !info.IsDir() {
+			if _, exists := bucketObjects[path]; !exists {
+				err := os.Remove(path)
+				if err != nil {
+					log.Fatalln(err)
+				}
+				fmt.Printf("Removed local file: %s\n", path)
+			}
+		}
+		return nil
+	})
+	if err != nil {
+		log.Fatalln(err)
 	}
 }
